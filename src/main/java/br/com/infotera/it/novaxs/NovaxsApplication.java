4f47be02f0c48b9cf5b6ca6f8b96b7ef191f6534
lucas <lucas@infotera.com.br>
2022-01-06 10:32:06 -0300

    First commit

---
+++ src/main/java/br/com/infotera/it/novaxs/NovaxsApplication.java
@@ -0,0 +1,13 @@
+package br.com.infotera.it.novaxs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaxsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovaxsApplication.class, args);
+	}
+
+}
